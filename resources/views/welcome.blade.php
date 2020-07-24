265e339a78ca979e9058bda01b116182203a0574
sumanta <chattu.d@gmail.com>
2020-07-24 23:10:08 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    
+</head>
+<body>
+    <div id="app">
+        
+            <example-component></example-component>
+        
+
+    </div>
+</body>
+    <script src="{{asset('js/app.js')}}"></script>
+</html>
